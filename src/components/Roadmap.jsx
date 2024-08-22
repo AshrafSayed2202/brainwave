2b8d18b0c1751cab7632b7558e80b2febb285d86
Ashraf Sayed <ashraf.neizk@gmail.com>
2024-08-22 16:46:50 +0300

    Services Section Done

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,7 +1,7 @@
 import Button from "./Button";
 import Heading from "./Heading";
 import Section from "./Section";
-import Tagline from "./TagLine";
+import Tagline from "./Tagline";
 import { roadmap } from "../constants";
 import { check2, grid, loading1 } from "../../public/assets";
 import { Gradient } from "./design/Roadmap";
@@ -16,9 +16,8 @@ const Roadmap = () => (
                         item.status === "done" ? "Done" : "In progress";
                     return (
                         <div
-                            className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${
-                                item.colorful ? "bg-conic-gradient" : "bg-n-6"
-                            }`}
+                            className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${item.colorful ? "bg-conic-gradient" : "bg-n-6"
+                                }`}
                             key={item.id}
                         >
                             <div className="relative p-8 bg-n-8 rounded-[2.4375rem] overflow-hidden xl:p-15">
